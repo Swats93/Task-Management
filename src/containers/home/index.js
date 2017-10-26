1c9f6fd2a262d0949bf17ef40735aa21c612441e
Swats93 <garg.swati93@gmail.com>
2017-10-26 13:53:12 +0530

    first screen changes

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {css} from 'glamor';
 import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import tachyons from 'tachyons';
 import {
   increment,
   incrementAsync,
@@ -10,21 +12,25 @@ import {
 } from '../../modules/counter'
 
 const Home = props => (
-  <div>
-    <h1>Home</h1>
-    <p>Count: {props.count}</p>
+  <div className="w-100 h-100 fl-ns cf bg-pink">
+    <div className="mw8 mt5 mb5 center pb4 bg-white">
+      <div className="tc f3 pt4">Final Quadrant Task Management</div>
+      <div className="tc mt5 f4 b">Welcome Back !</div>
+      <div className="mt4 mw5 pb5 center">
+        <div className="f5 b">Email</div>
+        <div className="mt2">
+          <input className="w-100" placeholder="Enter your email" type="text" {...css({height: '8%'})}/>
+        </div>
+        <div className="f5 b mt3">Password</div>
+        <div className="mt2">
+          <input className="w-100" placeholder="Enter your password" type="password" {...css({height: '8%'})} />
+        </div>
+        <div className="mt3 pb5">
+          <a className="w-100 pointer f5 link dim bg-pink tc pv2 b dib white">Login</a>
+        </div>
+      </div>
 
-    <p>
-      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
-      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
-    </p>
-
-    <p>
-      <button onClick={props.decrement} disabled={props.isDecrementing}>Decrement</button>
-      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
-    </p>
-
-    <p><button onClick={() => props.changePage()}>Go to about page via redux</button></p>
+    </div>
   </div>
 )
 
